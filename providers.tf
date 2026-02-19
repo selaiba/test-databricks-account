a41f9d4f9499f8e44c8663359a263c0d0a99132f
Salem Elaiba <salem.elaiba@hotmail.com>
2026-02-19 12:47:43 +0100

    update providers tf

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
-provider "databricks" {
-  alias      = "account"
+#provider "databricks" {
+  #alias      = "account"
   #host       = var.databricks_host
   #account_id = var.databricks_account_id
-}
+#}
 
 provider "azurerm" {
   features {}
